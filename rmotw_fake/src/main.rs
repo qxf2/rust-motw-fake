a8eaf3711b0398e6b910d36363856046bac9d107
Archana-GS <gsarchana93@gmail.com>
2023-11-16 14:06:33 +0530

    usecases to generate random numbers from seeded data

--- rmotw_fake/src/main.rs
+++ rmotw_fake/src/main.rs
@@ -2,6 +2,7 @@ use fake::{Dummy, Fake, Faker};
 use fake::faker::name::en::*;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
+use rand::prelude::*;
 
 #[derive(Debug, Dummy)]
 pub struct Profile {
@@ -14,13 +15,13 @@ pub struct Profile {
 
 fn dummy_profile() {
     let profile: Profile = Faker.fake();
-    println!("{:?}", profile);    
+    println!("{:?}", profile);
 }
 
 fn main() {
     let seed = [
-    1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-    0, 0, 0, 0,
+        1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0,
     ];
     let ref mut r = StdRng::from_seed(seed);
     for _ in 0..2 {
@@ -28,4 +29,23 @@ fn main() {
         println!("value from fixed seed {}", v);
     }
     dummy_profile();
+
+    if rand::random() {
+        println!("Random boolean is true");
+        // Try printing a random unicode code point (probably a bad idea)!
+        println!("Random char: {}", rand::random::<char>());
+    } else {
+        println!("Random boolean is false");
+    }
+
+    // Random float between 0 and 1
+    let mut rng = rand::thread_rng();
+    let y: f64 = rng.gen();
+    println!("Random float: {}", y);
+
+    // Shuffled vector of integers
+    let mut nums: Vec<i32> = (1..5).collect();
+    nums.shuffle(&mut rng);
+    println!("Shuffled vector: {:?}", nums);
+    
 }
