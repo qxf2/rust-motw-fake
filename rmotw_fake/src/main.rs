18ab9ac7c5aa57b14dc2e46e925abf82c2a48381
Archana-GS <gsarchana93@gmail.com>
2023-12-11 14:06:43 +0530

    adding seeded data to generate fake profiles

--- rmotw_fake/src/main.rs
+++ rmotw_fake/src/main.rs
@@ -1,8 +1,11 @@
-use fake::{Dummy, Fake, Faker};
+use fake::{Dummy, Faker, Fake};
 use fake::faker::name::en::*;
+use fake::faker::address::en::StreetName;
+use fake::faker::phone_number::en::*;
+use fake::faker::internet::en::SafeEmail;
+use fake::faker::company::en::CompanyName;
 use rand::rngs::StdRng;
 use rand::SeedableRng;
-use rand::prelude::*;
 
 #[derive(Debug, Dummy)]
 pub struct Profile {
@@ -11,41 +14,34 @@ pub struct Profile {
 
     #[dummy(faker = "18..80")]
     pub age: usize,
+
+    #[dummy(faker = "StreetName()")]
+    pub address: String,
+
+    #[dummy(faker = "PhoneNumber()")]
+    pub phone_number: String,
+
+    #[dummy(faker = "SafeEmail()")]
+    pub email: String,
+
+    #[dummy(faker = "CompanyName()")]
+    pub company: String,
 }
 
-fn dummy_profile() {
-    let profile: Profile = Faker.fake();
+fn dummy_profile(rng: &mut StdRng) {
+    let profile: Profile = Faker.fake_with_rng(rng);
     println!("{:?}", profile);
 }
 
 fn main() {
     let seed = [
-        1, 0, 0, 0, 23, 0, 0, 0, 200, 1, 0, 0, 210, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 3, 1, 0, 0, 2, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
         0, 0, 0, 0,
     ];
-    let ref mut r = StdRng::from_seed(seed);
+
+    let mut rng = StdRng::from_seed(seed);
+
     for _ in 0..2 {
-        let v: usize = Faker.fake_with_rng(r);
-        println!("value from fixed seed {}", v);
+        dummy_profile(&mut rng);
     }
-    dummy_profile();
-
-    if rand::random() {
-        println!("Random boolean is true");
-        // Try printing a random unicode code point (probably a bad idea)!
-        println!("Random char: {}", rand::random::<char>());
-    } else {
-        println!("Random boolean is false");
-    }
-
-    // Random float between 0 and 1
-    let mut rng = rand::thread_rng();
-    let y: f64 = rng.gen();
-    println!("Random float: {}", y);
-
-    // Shuffled vector of integers
-    let mut nums: Vec<i32> = (1..5).collect();
-    nums.shuffle(&mut rng);
-    println!("Shuffled vector: {:?}", nums);
-    
 }
