59f4acf513e5b281cc59bb97f35c8fae7223dfba
Sravanti <sravanti.tatiraju@qxf2.com>
2023-12-14 22:52:12 +0530

    Structured the examples

---
+++ rmotw_fake/src/bin/fake_example3.rs
@@ -0,0 +1,53 @@
+//This example shows how to use Dummy Trait for generating fake data. This is useful for data structures
+
+use fake::faker::boolean::en::*;
+use fake::faker::name::en::*;
+use fake::Dummy;
+use fake::{Fake, Faker};
+
+#[derive(Debug, Dummy)]
+pub enum PostStatus {
+    Published,
+    Draft,
+}
+
+#[derive(Debug, Dummy)]
+pub struct SocialMediaPost {
+    #[dummy(faker = "1000..")]
+    pub post_id: usize,
+
+    #[dummy(faker = "Name()")]
+    pub author: String,
+
+    #[dummy(faker = "(Faker, 3..5)")]
+    pub comments: Vec<Comment>,
+
+    #[dummy(faker = "Boolean(80)")]
+    pub is_public: bool,
+
+    pub status: PostStatus,
+
+}
+
+#[derive(Debug, Dummy)]
+pub struct Comment {
+    #[dummy(faker = "1..100")]
+    pub comment_id: usize,
+
+    pub text: String,
+
+    #[dummy(faker = "Name()")]
+    pub commenter: String,
+
+}
+
+pub fn generate_fake_post() {
+    let post: SocialMediaPost = Faker.fake();
+    println!("{:#?}", post);  
+    //let posts = fake::vec![SocialMediaPost; 3];
+    //println!("{:?}", posts)
+}
+
+pub fn main() {
+    generate_fake_post();
+}
