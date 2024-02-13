6822971aaad2e1fca47a5105c98f01a310439413
Archana-GS <gsarchana93@gmail.com>
2024-02-13 18:53:32 +0530

    code for optional struct usage

--- rmotw_fake/src/bin/optional_struct_usage.rs
+++ rmotw_fake/src/bin/optional_struct_usage.rs
@@ -1,28 +1,29 @@
 //Example program that shows how to use optional struct while generating fake data
 
-use fake::{Dummy, Fake, Faker, Opt, Optional};
-use fake::faker::name::en::*;
+use fake::{Dummy, Fake, Faker};
+use fake::faker::name::en::Name;
 
 #[derive(Debug, Dummy)]
 pub struct Profile {
     #[dummy(faker = "Name()")]
     pub name: String,   
 
-    #[dummy(faker = "Opt(0..80, 50)", from = "Optional<u64>")]
+    #[dummy(faker = "0..=80")]
     pub age: Option<u64>,
 
-    #[dummy(faker = "Opt(0..200, 100)", from = "Optional<u64>")]
+    #[dummy(faker = "0..=200")]
     pub following: Option<u64>,
 
-    #[dummy(expr = "Some((0..200).fake())")]
+    #[dummy(expr = "Some((0..=200).fake())")]
     pub followers: Option<u64>,
 
-    #[dummy(faker = "0..200")]
+    #[dummy(faker = "0..=200")]
     pub posts: Option<Option<u64>>,
 }
 
 pub fn print_profile(profile: Profile) {
-    println!("Name: {}", profile.name);
+    println!("Profile {{\nname: \"{}\",\nage: {:?},\nfollowing: {:?},\nfollowers: {:?},\nposts: {:?}\n}}",
+    profile.name, profile.age, profile.following, profile.followers, profile.posts);
     match profile.age {
         Some(age) => println!("Age: {}", age),
         None => println!("Age: not provided"),
@@ -35,10 +36,16 @@ pub fn print_profile(profile: Profile) {
         Some(following) => println!("Following: {}", following),
         None => println!("Following: not provided"),
     }
+    match profile.posts {
+        Some(posts) => match posts {
+            Some(value) => println!("Posts: {}", value),
+            None => println!("Posts: not provided"),
+        },
+        None => println!("Posts: not provided"),
+    }
 }
 
 fn main() {
     let profile: Profile = Faker.fake();
-    println!("{:?}", profile);
     print_profile(profile)
 }
