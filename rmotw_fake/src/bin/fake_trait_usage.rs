7fa3b10573cc564a0b2276126b0fc9d1d690b2ec
Archana-GS <gsarchana93@gmail.com>
2024-02-13 18:52:41 +0530

    code for fake trait usage

--- rmotw_fake/src/bin/fake_trait_usage.rs
+++ rmotw_fake/src/bin/fake_trait_usage.rs
@@ -2,20 +2,20 @@
 
 use fake::Fake;
 
-fn generate_fake_social_media_data() {
+fn generate_fake_social_media_data() -> (u32, u16, String, String) {
     let fake_likes = (1..1000).fake::<u32>();
-    println!("Number of Likes: {}", fake_likes);
-
     let fake_shares = (1..100).fake::<u16>();
-    println!("Number of Shares: {}", fake_shares);
-
     let fake_post_content = (5..20).fake::<String>();
-    println!("Generated Post Content: {:?}", fake_post_content);
-
     let fake_comment = (2..5).fake::<String>();
-    println!("Generated Comment: {:?}", fake_comment);
+
+    (fake_likes, fake_shares, fake_post_content, fake_comment)
 }
 
 fn main() {
-    generate_fake_social_media_data();
+    let (fake_likes, fake_shares, fake_post_content, fake_comment) = generate_fake_social_media_data();
+
+    let output = format!("Number of Likes: {}\nNumber of Shares: {}\nGenerated Post Content: {:?}\nGenerated Comment: {:?}", 
+    fake_likes, fake_shares, fake_post_content, fake_comment);
+
+    println!("{}", output);
 }
